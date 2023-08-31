4f70bcd55560f2fd1173df73d25b62512bbd3d1a
Aye7 <mr.aissat@gmail.com>
2023-09-01 00:04:45 +0100

    added `docker_env` && `docker_env_file`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,22 @@
 #!/usb/bin/env bash
 set -e
 
+if [ -z "$DOCKER_ENV" ]; then
+  DOCKER_ENV=''
+fi
+
+if [ -z "$DOCKER_ENV_FILE" ]; then
+  DOCKER_ENV_FILE=''
+fi
+echo "========================================================================"
+echo $DOCKER_ENV
+echo $DOCKER_ENV_FILE
+
+echo -n "$DOCKER_ENV_FILE" | base64 -d > .env
+cat .env
+
+echo "========================================================================"
+
 log() {
   echo ">> [local]" $@
 }
@@ -20,12 +36,16 @@ tar cjvf /tmp/workspace.tar.bz2 --exclude .git .
 log "Launching ssh agent."
 eval `ssh-agent -s`
 
-remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace\" -xjv ; log 'Launching docker compose...' ; cd \"\$HOME/workspace\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
+# Construct the remote_command with Docker environment variables
+remote_command="set -e ; $DOCKER_ENV  log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace\" -xjv ;"
 if $PULL; then
-  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace\" -xjv ; log 'Launching docker compose...' ; cd \"\$HOME/workspace\" ; log 'Pull images...' ; docker compose pull ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
+  remote_command="$remote_command log 'Launching docker compose...' ; cd \"\$HOME/workspace\" ; log 'Pull images...' ; docker compose pull ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build ;"
+else
+  remote_command="$remote_command log 'Launching docker compose...' ; cd \"\$HOME/workspace\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build ;"
 fi
-if $USE_DOCKER_STACK ; then
-  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" -xjv ; log 'Launching docker stack deploy...' ; cd \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" ; docker stack deploy -c \"$DOCKER_COMPOSE_FILENAME\" --prune \"$DOCKER_COMPOSE_PREFIX\""
+
+if $USE_DOCKER_STACK; then
+  remote_command="$remote_command log 'Launching docker stack deploy...' ; cd \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" ; docker stack deploy -c \"$DOCKER_COMPOSE_FILENAME\" --prune \"$DOCKER_COMPOSE_PREFIX\" ;"
 fi
 
 #
